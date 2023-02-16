fa0384cdca0a673b155f230cece92c97e2ac0d16
Радмир Даминов <daminov.r@phystech.edu>
2023-02-16 17:06:12 +0300

    objectivecontroller done

--- CameraQHYCCD/include/objectivecontroller.h
+++ CameraQHYCCD/include/objectivecontroller.h
@@ -1,12 +1,13 @@
 #ifndef OBJECTIVECONTROLLER_H
 #define OBJECTIVECONTROLLER_H
 #include <iostream>
+#include "serialib.h"
 
 using namespace std;
 
 class ObjectiveController {
 public:
-    explicit ObjectiveController(const string& pattern, int32_t port = -1);
+    explicit ObjectiveController(const string& pattern);
 
     void setDiaphragmLevel(const string& value);
 
@@ -16,13 +17,14 @@ public:
 
     string currentError() const;
 
-    void connectToController(const string& ptrn, int32_t port = -1);
+    bool connectToController(const string& serialPort);
 
 private:
 
     bool testControllerActive();
-    string pattern;
-    string objective;
+
+    serialib mObjective;
+    string mSerialPort;
     string error;
     static constexpr const int16_t focusCommandSize = 6;
     static constexpr const int16_t diaphragmCommandSize = 4;
--- CameraQHYCCD/src/objectivecontroller.cpp
+++ CameraQHYCCD/src/objectivecontroller.cpp
@@ -1,125 +1,69 @@
 #include "objectivecontroller.h"
 
-ObjectiveController::ObjectiveController(const string& pattern, int32_t port) : pattern(pattern) {
-     connectToController(pattern, port);
+ObjectiveController::ObjectiveController(const string& serialPort) : mSerialPort(serialPort) {
+     connectToController(pattern);
 }
 
-bool ObjectiveController::testControllerActive() {
-    if (!objective.isOpen())
-    {
-        qDebug() << "контроллер не активирован";
+bool ObjectiveController::connectToController(const string& serialPort) {
+    error.clear();
+
+    if(mObjective.isDeviceOpen())
+        mObjective.closeDevice();
+
+    if( mObjective.openDevice(serialPort, 38400) != 1 ) {
+        error = "Can't open connection on current port";
         return false;
     }
     return true;
 }
 
-void ObjectiveController::setDiaphragmLevel(const QString& value) {
+bool ObjectiveController::testControllerActive() {
+    if(mObjective.isDeviceOpen())
+        return true;
+    return false;
+}
+
+void ObjectiveController::setDiaphragmLevel(const string& value) {
     error.clear();
-    if (testControllerActive())
-    {
-        if (value.size() == diaphragmCommandSize && value.at(0) == 'A' && value.at(value.size() - 1) == '#')
-        {
-            if (objective.write(value.toLocal8Bit())  == -1)
-            {
-                error = objective.errorString();
-                qDebug() << error;
+    if (testControllerActive()) {
+        if (value.size() == diaphragmCommandSize && value.at(0) == 'A' && value.at(value.size() - 1) == '#') {
+            if( mObjective.writeString(value) != 1 ) {
+                error = "Writting error";
             }
         }
         else
-        {
             error = "Wrong command format";
-            qDebug() << error;
-        }
     }
 }
 
-void ObjectiveController::setFocusing(const QString& value)
-{
+void ObjectiveController::setFocusing(const string& value) {
     error.clear();
-    qDebug() << "OBJECTIVE" << value;
-    if (testControllerActive())
-    {
-        if (value.size() == focusCommandSize && value.at(0) == 'M' && value.at(value.size() - 1) == '#')
-        {
-            if (objective.write(value.toLocal8Bit()) == -1)
-            {
-                error = objective.errorString();
-                qDebug() << error;
+    if (testControllerActive()) {
+        if (value.size() == focusCommandSize && value.at(0) == 'M' && value.at(value.size() - 1) == '#') {
+            if( mObjective.writeString(value) != 1 ) {
+                error = "Writting error";
             }
         }
         else
-        {
             error = "Wrong command format";
-            qDebug() << error;
-        }
     }
 }
 
 string ObjectiveController::getCurrentFocusing() {
     error.clear();
-    if (testControllerActive())
-    {
-        if (objective.write(QString("P#").toLocal8Bit()))
-        {
-            if (objective.waitForReadyRead(1000))
-            {
-                QByteArray array = objective.read(objective.bytesAvailable());
-                return QString::fromLatin1(array.data());
+    string result = "";
+    if (testControllerActive()) {
+        mObjective.flushReceiver();
+        if(mObjective.writeString("P#") == 1) {
+            if( mObjective.readString(result, '#', 10, 1000) != 1 ) {
+                // TO DO: PARSE AND RETURN ABS VAL
+                return result;
             }
             else
-            {
                 error = "Can't get answer";
-                qDebug() << error;
-            }
-        }
-        else
-        {
-            error = "Wrong command format";
-            qDebug() << error;
         }
     }
-    return QString();
+    return result;
 }
 
 
-void ObjectiveController::connectToController(const string& ptrn, int32_t port) {
-    pattern = ptrn;
-    error.clear();
-    objective.close();
-
-    if (port == -1)
-    {
-        auto ports = QSerialPortInfo::availablePorts();
-        for (auto& i : ports)
-        {
-            qDebug() << i.description() << i.manufacturer();
-        }
-        for (qint32 i = 0; i < ports.size(); ++i)
-        {
-           // qDebug() << ports.size() << ports[i].description() << ports[i].manufacturer();
-            if (ports[i].description().contains(pattern))
-            {
-                port = i;
-                break;
-            }
-        }
-    }
-    if (port != -1)
-    {
-        objective.setPort(QSerialPortInfo::availablePorts().at(port));
-        if (objective.open(QIODevice::ReadWrite))
-        {
-            objective.setBaudRate(38400);
-        }
-        else
-        {
-            error = "Can't open connection on current port " + objective.errorString();
-            qDebug() << error;
-        }
-    }
-    else
-    {
-        error = "Invalid port ";
-        qDebug() << error;
-    }
-}
